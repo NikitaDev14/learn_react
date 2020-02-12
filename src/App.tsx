04a2ddb1230142443b05b3fe0fa5de3790cad106
NikitaDev14 <mykytababych1989@gmail.com>
2020-02-12 21:37:08 +0200

    Added state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Person } from './components/person';
+import { Person as PersonModel } from './models';
 
-export class App extends Component {
-  render() {
+type PersonState = PersonModel[];
+
+export class App extends Component<{}, PersonState> {
+  public state: PersonState = [
+    { name: 'Max', age: 28 },
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 26 },
+  ];
+
+  private switchNameHandler() {
+    console.log('Clicked!');
+  }
+
+  public render() {
     return (
       <div className="App">
-        <Person name="Max" age={28}/>
-        <Person name="Manu" age={29}>My Hobbies: Racing</Person>
-        <Person name="Stephanie" age={26}/>
+        <h1>React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {this.state.map((person: PersonModel) => 
+          <Person name={person.name} age={person.age}/>
+        )}
       </div>
     );
   }

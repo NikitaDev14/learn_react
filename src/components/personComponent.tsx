a220b1a86ddeee6638d5736aec9c72aad63f7dd4
Nikita <nikita.babych@wearehathway.com>
2021-06-19 12:04:56 +0300

    Revert

--- src/components/personComponent.tsx
+++ src/components/personComponent.tsx
@@ -1,11 +1,10 @@
-import React, { ChangeEvent, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 
 import { Person } from '../models'
 import './personComponent.css'
 
-export const PersonComponent: FunctionComponent<Person & {click: () => void} & {changed: (event: ChangeEvent) => void}> = ({name, age, click, changed, children}) =>
+export const PersonComponent: FunctionComponent<Person> = ({name, age, children}) =>
   <div className="Person">
-    <p onClick={click}>I'm {name} and I'm {age} years old!</p>
     <p>{children}</p>
-    <input type="text" onChange={changed}/>
+    <input type="text"/>
   </div>

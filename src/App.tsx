8505e59ba106722ed6dc34d6f98794cacf22dc92
Nikita <nikita.babych@wearehathway.com>
2021-06-19 12:17:55 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,58 @@
-import React, { FunctionComponent, useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
-import { PersonComponent } from './components/personComponent';
-import { Person as PersonModel } from './models';
-
-interface PersonState {
-  persons: PersonModel[];
-}
-
-export const App: FunctionComponent = (props) => {
-  const [personsState, setPersonsState] = useState<PersonState>({
-    persons: [
-      { key: '1', name: 'Max', age: 28 },
-      { key: '2', name: 'Manu', age: 29 },
-      { key: '3', name: 'Stephanie', age: 26 },
-    ],
-  });
-  
-  // in function based component 'setState' hook does not merge state, it replaces it!
-  const switchNameHandler = () => {
-    // console.log('Clicked!');
-    setPersonsState({
-      persons: [
-        { key: '1', name: 'Maximilian', age: 28 },
-        { key: '2', name: 'Manu', age: 29 },
-        { key: '3', name: 'Stephanie', age: 30 },
-      ],
-    });
-  };
-  
+function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
-
-      <button onClick={switchNameHandler}>Switch Name</button>
-
-      {personsState.persons.map((person: PersonModel) =>
-        <PersonComponent key={person.key} name={person.name} age={person.age}/>
-      )}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
-};
+}
 
-
+export default App;

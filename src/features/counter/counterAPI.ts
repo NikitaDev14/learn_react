92b489506a72ac0c8982343c4d3fbcef9ac39383
Nikita <nikita.babych@wearehathway.com>
2022-05-15 13:27:49 +0400

    SStyles

--- src/features/counter/counterAPI.ts
+++ src/features/counter/counterAPI.ts
@@ -1,6 +1,12 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
   return new Promise<{ data: number }>((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+    setTimeout(() => resolve({ data: amount }), 1000)
   );
+}
+
+export enum LOADING_STATUS {
+  Idle,
+  Loading,
+  Failed,
 }
